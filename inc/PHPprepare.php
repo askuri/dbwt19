57c39b22da94cebfe5231b8b16fa7ebd4e4aed3a
askuri <enwi2@t-online.de>
2019-11-22 16:15:07 +0100

    Make PHPprepare point to proper .env
    .env files are supposed to be kept in the root of the project,
    no need to keep them somewhere else. It's also already listed in
    .gitignore

--- inc/PHPprepare.php
+++ inc/PHPprepare.php
@@ -1,8 +1,6 @@
 <?php
 require 'vendor/autoload.php';
 
-//TODO Change to correct path
-// $dotenv = Dotenv\Dotenv::create('/home/martin/NetBeansProjects/dbwt19');
-$dotenv = Dotenv\Dotenv::create('/home/leonhard/Code/dbwt19');
+$dotenv = Dotenv\Dotenv::create('./');
 $dotenv->load();
 $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS','DB_PORT']);
