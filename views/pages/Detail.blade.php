c247089237b368658c374af7ebda9fc7203342cd
askuri <enwi2@t-online.de>
2019-11-25 17:06:50 +0100

    Move Login to own include
    We might need this on other pages later.

--- views/pages/Detail.blade.php
+++ views/pages/Detail.blade.php
@@ -15,18 +15,7 @@
             <!-- login -->
             <div class="col-3">
                 <aside>
-                    <fieldset class="form-group border p-2">
-                        <legend class="col-form-label w-auto">Login</legend>
-                        <form method="post" action="#">
-                            <div class="form-group">
-                                <input type="text" class="form-control form-control-sm" id="inputUser" placeholder="Benutzer">
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control form-control-sm" id="inputPassword" placeholder="******">
-                            </div>
-                            <button type="submit" class="btn btn-link">Anmelden</button>
-                        </form>
-                    </fieldset>
+                    @include('includes.Login')
                 </aside>
             </div>
 
